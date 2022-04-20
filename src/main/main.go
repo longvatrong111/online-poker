2216a9073e5167fc6172c0ffc7c8c0f6078cec97
trong.hoang <trong.hoang@lge.com>
2022-04-20 18:19:46 +0700

    http tutorial

--- src/main/main.go
+++ src/main/main.go
@@ -15,5 +15,19 @@ func main() {
 
 func handleHttp(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("HTTP request headers\n")
-	
+	for name, values := range r.Header {
+		for _, value := range values {
+			fmt.Printf(name, value)
+		}
+	}
+	fmt.Printf("\nHTTP request method: %s \n", r.Method)
+	fmt.Printf("HTTP request body: %s \n", r.Body)
+	fmt.Printf("-----------------------------------\n")
+
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, "<div><h1>Hello World</h1><a>fb.com</a></div>")
+	default:
+		fmt.Fprintf(w, "only get")
+	}
 }
